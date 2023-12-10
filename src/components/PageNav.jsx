9ae72fd01b99560da5781921b23a097b4a6eb81c
Ferperemero86 <fernandoperezmerono@gmail.com>
2023-12-10 16:29:54 +0000

    Feature: Create app pages

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,19 +1,27 @@
 import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import styles from "./PageNav.module.scss";
 
-export default function PageNav() {
+function PageNav() {
   return (
-    <nav>
+    <nav className={styles.nav}>
+      <Logo />
+
       <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
-          <NavLink to="/">Product</NavLink>
+          <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login" className={styles.ctaLink}>
+            Login
+          </NavLink>
         </li>
       </ul>
     </nav>
   );
 }
+
+export default PageNav;
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,11 @@
+import styles from "./AppLayout.module.scss";
+
+function AppLayout() {
+  return (
+    <div className={styles.app}>
+      <h1>Layout</h1>
+    </div>
+  );
+}
+
+export default AppLayout;
