32a8d174e93b297937828824980d9244d0cff9d4
Ferperemero86 <fernandoperezmerono@gmail.com>
2023-12-17 13:27:20 +0000

    Feature: Add map pinpoints

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,12 +1,15 @@
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useCities } from "@/contexts/CitiesContext";
 
 import styles from "@/components/Map/Map.module.scss";
-import { useState } from "react";
 
 export default function Map() {
   const navigate = useNavigate();
+
+  const { cities } = useCities();
   const [mapPosition, setMapPosition] = useState([40, 0]);
 
   return (
@@ -26,11 +29,17 @@ export default function Map() {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={mapPosition}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
       </MapContainer>
     </div>
   );
